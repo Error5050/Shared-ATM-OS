a0658212a0f15e57b1dcd18527526ed6c3315c41
Error5050 <123643651+Error5050@users.noreply.github.com>
2026-04-16 12:54:40 +0200

    Create SyncTest.java

---
+++ SyncTest.java
@@ -0,0 +1,26 @@
+public class SyncTest {
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount();
+
+        Runnable task = () -> {
+            for (int i = 0; i < 100; i++) {
+                account.withdrawSync(10);
+            }
+        };
+
+        Thread t1 = new Thread(task);
+        Thread t2 = new Thread(task);
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Final Balance (Sync): " + account.getBalance());
+    }
+}
